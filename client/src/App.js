389c6550a75895918b9f402c8ac44f1f2809a82b
박상혁 <shplove4568@gmail.com>
2021-04-21 14:59:27 +0900

    make Component for add Customer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Component } from 'react';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import {Table, TableHead, TableBody, TableRow, TableCell, Paper, CircularProgress} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -33,28 +34,31 @@ class App extends Component {
   render () {
     const { classes } = this.props
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers ? this.state.customers.map( (c) => { return <Customer key={c.ID} id={c.ID} image={c.IMAGE} name={c.NAME} gender={c.GENDER} job={c.JOB}/> }) :
-            <TableRow>
-              <TableCell colSpan="5" align="center">
-                <CircularProgress />
-              </TableCell>
-            </TableRow>
-            }
-          </TableBody>
-        </Table>
-      </Paper>
+      <>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? this.state.customers.map( (c) => { return <Customer key={c.ID} id={c.ID} image={c.IMAGE} name={c.NAME} gender={c.GENDER} job={c.JOB}/> }) :
+              <TableRow>
+                <TableCell colSpan="5" align="center">
+                  <CircularProgress />
+                </TableCell>
+              </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd/>
+      </>
     )
   }
   
